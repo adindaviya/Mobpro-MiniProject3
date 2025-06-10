684b083a41de0c1923a2ab67a69eaae8ef33a310
Adinda Alif Viya <adindaalifviya@gmail.com>
2025-06-10 14:51:31 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Assessment 3
+
+Nama: Adinda Alif Viya
+
+NIM: 607062300052
---
+++ app/src/main/java/com/adindaviya0052/minipoject3/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.adindaviya0052.minipoject3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.adindaviya0052.minipoject3.ui.theme.MiniPoject3Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MiniPoject3Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MiniPoject3Theme {
+        Greeting("Android")
+    }
+}
